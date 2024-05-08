47f308d9fcfa9749167e9d0733c02cdaa2b17b15
MaicolSmith <maicol.smith.bueno@gmail.com>
2024-05-07 21:40:35 -0500

    respuesta5

---
+++ Ejercicio1/respuesta5.js
@@ -0,0 +1,16 @@
+//Escribe una función para calcular el área de un círculo. Puedes utilizar funciones declarativas.
+
+function area_de_un_ciculo(radio) {
+    const pi = Math.PI;
+    const area = pi * radio * radio;
+    return area;
+  }
+  
+  // Example usage:
+  const radio = 5;
+  const area_de_un_ciculovalor = area_de_un_ciculo(radio);
+  console.log(`el area de un circulo es: ${radio} is ${area_de_un_ciculovalor}.`);
+
+
+
+
